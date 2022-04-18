5ce1403d82f23fffd4b9af8011713989f13b0177
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-04-18 14:32:52 +0530

    added list by user

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,21 +1,23 @@
 package com.addressbook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book program");
 
+        Scanner scanner = new Scanner(System.in);
         Contact contact1 = new Contact();
-        contact1.firstName = "Kota";
-        contact1.lastName = "Sukumar";
-        contact1.address = "1-1, xyz street";
-        contact1.city = "CTR";
-        contact1.state = "AP";
-        contact1.zip = 123456;
-        contact1.contactNumber = 1234567895;
-        contact1.emailID = "xyz@gmail.com";
+        contact1.firstName = scanner.nextLine();
+        contact1.lastName = scanner.nextLine();
+        contact1.address = scanner.nextLine();
+        contact1.city = scanner.nextLine();
+        contact1.state = scanner.nextLine();
+        contact1.zip = scanner.nextLong();
+        contact1.contactNumber = scanner.nextLong();
+        contact1.emailID = scanner.nextLine();
 
         ContactStorage contactStorage = new ContactStorage();
         contactStorage.add(contact1);
